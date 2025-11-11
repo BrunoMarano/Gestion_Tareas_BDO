7b55ab01d2146f4e6d7fc00092c3a66fbcf8865c
EnzoBarrios <EnzoBarrios2018@gmail.com>
2025-11-11 16:50:20 -0300

    Modificacion de los CONSTRAINTS de las fechas

--- script/script_ddl_proyecto.sql
+++ script/script_ddl_proyecto.sql
@@ -1,5 +1,7 @@
 -- SCRIPT TEMA "nombre del tema"
 -- DEFINNICIÓN DEL MODELO DE DATOS
+DROP DATABASE gestion_proyecto;
+
 CREATE DATABASE gestion_proyecto;
 GO
 
@@ -22,10 +24,9 @@ CREATE TABLE proyecto
   id_proyecto INT IDENTITY(1,1) NOT NULL,
   nombre VARCHAR(100) NOT NULL,
   descripcion VARCHAR(200),
-  fecha_creacion DATE NOT NULL,      
+  fecha_creacion DATE NOT NULL CONSTRAINT DF_proyecto_fecha_creacion DEFAULT GETDATE(),   
   fecha_fin DATE,
   CONSTRAINT PK_proyecto PRIMARY KEY (id_proyecto),
-  CONSTRAINT DF_proyecto_fecha_creacion DEFAULT GETDATE(),
   CONSTRAINT CK_proyecto_fechas CHECK (fecha_creacion <= fecha_fin OR fecha_fin IS NULL)
 );
 GO
@@ -43,7 +44,7 @@ CREATE TABLE tarea
   id_tarea INT IDENTITY(1,1) NOT NULL,
   titulo VARCHAR(150) NOT NULL,
   descripcion VARCHAR(200),
-  fecha_creacion DATE NOT NULL, 
+  fecha_creacion DATE NOT NULL CONSTRAINT DF_tarea_fecha_creacion DEFAULT GETDATE(), 
   fecha_vencimiento DATE,
   id_proyecto INT NOT NULL,
   id_responsable INT NOT NULL,
@@ -52,7 +53,6 @@ CREATE TABLE tarea
   CONSTRAINT FK_tarea_proyecto FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto),
   CONSTRAINT FK_tarea_usuario FOREIGN KEY (id_responsable) REFERENCES usuario(id_usuario),
   CONSTRAINT FK_tarea_estado FOREIGN KEY (id_estado) REFERENCES estado(id_estado),
-  CONSTRAINT DF_tarea_fecha_creacion DEFAULT GETDATE(),
   CONSTRAINT CK_tarea_fechas CHECK (fecha_creacion <= fecha_vencimiento OR fecha_vencimiento IS NULL)
 );
 GO
@@ -77,12 +77,11 @@ CREATE TABLE comentario
 (
   id_comentario INT IDENTITY(1,1) NOT NULL,
   descripcion VARCHAR(200) NOT NULL,
-  fecha_comentario DATE NOT NULL,		
+  fecha_comentario DATE NOT NULL CONSTRAINT DF_comentario_fecha_comentario DEFAULT GETDATE(),		
   id_tarea INT NOT NULL,
   id_usuario INT NOT NULL,
   CONSTRAINT PK_comentario PRIMARY KEY (id_comentario),
   CONSTRAINT FK_comentario_tarea FOREIGN KEY (id_tarea) REFERENCES tarea(id_tarea),
-  CONSTRAINT DF_comentario_fecha_comentario DEFAULT GETDATE(),
   CONSTRAINT FK_comentario_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 GO
@@ -111,3 +110,4 @@ GO
 SELECT name 
 FROM sys.tables
 ORDER BY name;
+
