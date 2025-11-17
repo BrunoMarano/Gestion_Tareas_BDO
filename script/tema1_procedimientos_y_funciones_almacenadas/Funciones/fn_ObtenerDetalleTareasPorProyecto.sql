1bf53335d9c6e723275bafba5339f000ed3bd7f1
EnzoBarrios <EnzoBarrios2018@gmail.com>
2025-11-17 18:37:19 -0300

    Implementacion de funcion para obtener un listado de tareas por proyecto

---
+++ script/tema1_procedimientos_y_funciones_almacenadas/Funciones/fn_ObtenerDetalleTareasPorProyecto.sql
@@ -0,0 +1,26 @@
+USE gestion_proyecto;
+GO
+
+--  Muestra un listado detallado de las tareas de un proyecto específico.
+CREATE FUNCTION fn_ObtenerDetalleTareasPorProyecto (
+    @id_proyecto INT  -- El parámetro seguido del tipo de dato
+)
+RETURNS TABLE -- El tipo de valor que retorna
+AS
+RETURN
+(
+    SELECT
+        t.id_tarea,
+        t.titulo AS 'Titulo de la Tarea',
+        u.nombre AS 'Nombre del Responsable',
+        e.tipo_estado AS 'Estado'
+    FROM
+        tarea AS t
+    INNER JOIN
+        usuario AS u ON (t.id_responsable = u.id_usuario)
+    INNER JOIN
+        estado AS e ON (t.id_estado = e.id_estado)
+    WHERE
+        t.id_proyecto = @id_proyecto
+);
+GO
