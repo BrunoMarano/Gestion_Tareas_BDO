1378edba787994c4833d458f38ad2be697ed9822
EnzoBarrios <EnzoBarrios2018@gmail.com>
2025-11-17 18:31:52 -0300

    Documentacion de la funcion para contar los dias para el vencimiento de una tarea

--- script/tema1_procedimientos_y_funciones_almacenadas/Funciones/fn_DiasParaVencimiento.sql
+++ script/tema1_procedimientos_y_funciones_almacenadas/Funciones/fn_DiasParaVencimiento.sql
@@ -1,15 +1,17 @@
 USE gestion_proyecto;
 GO
 
+--  Calcula los días restantes para la fecha de vencimiento de una tarea.
 CREATE FUNCTION fn_DiasParaVencimiento (
-    @fecha_vencimiento DATE
+    @fecha_vencimiento DATE  -- El parámetro seguido del tipo de dato
 )
-RETURNS INT
+RETURNS INT -- El tipo de valor que retorna
 AS
 BEGIN
+    -- Manejo de nulos: si no hay fecha, no se puede calcular
     IF @fecha_vencimiento IS NULL
         RETURN NULL;
-
-    RETURN DATEDIFF(DAY, GETDATE(), @fecha_vencimiento);
+        
+    RETURN DATEDIFF(DAY, GETDATE(), @fecha_vencimiento);  --  Realiza el cálculo
 END;
 GO;
