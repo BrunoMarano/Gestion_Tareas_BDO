273c1d2f28aefc4e807b69e9ade44faae0c02173
Franco <francobdz@gmail.com>
2025-11-17 18:04:10 -0300

    Ajustes del script tema 4

--- script/tema4_vistas_vistas_indexadas/tema4.sql
+++ script/tema4_vistas_vistas_indexadas/tema4.sql
@@ -1,19 +1,3 @@
-CREATE DATABASE proyecto_gestion_cadetes;
-GO
-
-USE proyecto_gestion_cadetes;
-GO
-
-CREATE TABLE usuario
-(
-  id_usuario INT NOT NULL,
-  nombre VARCHAR(50) NOT NULL,
-  email VARCHAR(200) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id_usuario),
-  UNIQUE (email)
-);
-GO
 
 --Crear una vista sobre alguna tabla que solo muestre algunos campos de la misma.
 CREATE VIEW vista_usuario AS 
@@ -24,7 +8,7 @@ SELECT
 FROM usuario;
 GO
 
---Realizar insert de un lote de datos sobre la vista recién creada. 
+--Realizar insert de un lote de datos sobre la vista reciï¿½n creada. 
 ALTER TABLE usuario
 ADD CONSTRAINT df_usuario_password DEFAULT 'temporal123' FOR password;
 GO
@@ -43,7 +27,7 @@ GO
 
 --Realizar update sobre algunos de los registros creados 
 UPDATE vista_usuario
-SET nombre = 'Franco Benjamín Díaz'
+SET nombre = 'Franco Benjamï¿½n Dï¿½az'
 WHERE email = 'francobdz@gmail.com';
 GO
 
@@ -51,7 +35,7 @@ GO
 SELECT * FROM usuario;
 GO
 
---Borrar todos los registros insertados a través de la vista.
+--Borrar todos los registros insertados a travï¿½s de la vista.
 DELETE FROM vista_usuario
 WHERE id_usuario > 0;
 GO
@@ -59,7 +43,7 @@ GO
 SELECT * FROM vista_usuario;
 GO
 
---Crear un índice sobre alguna de las columnas sobre la vista  recién creada.
+--Crear un ï¿½ndice sobre alguna de las columnas sobre la vista  reciï¿½n creada.
 
 CREATE VIEW vista_usuario_indexada WITH SCHEMABINDING 
 AS
@@ -71,83 +55,4 @@ CREATE UNIQUE CLUSTERED INDEX IX_vista_usuario_indexada_id ON vista_usuario_inde
 GO
 
 -----------------------------------------------------------------
-
-CREATE TABLE proyecto
-(
-  id_proyecto INT NOT NULL,
-  nombre VARCHAR(100) NOT NULL,
-  descripcion VARCHAR(200),
-  fecha_creacion DATE NOT NULL,
-  fecha_fin DATE,
-  PRIMARY KEY (id_proyecto)
-);
-
-CREATE TABLE estado
-(
-  id_estado INT NOT NULL,
-  tipo_esatdo VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id_estado)
-);
-
-CREATE TABLE tarea
-(
-  id_tarea INT NOT NULL,
-  titulo VARCHAR(150) NOT NULL,
-  descripcion VARCHAR(200),
-  fecha_creacion DATE NOT NULL,
-  fecha_vencimiento DATE,
-  id_proyecto INT NOT NULL,
-  id_responsable INT NOT NULL,
-  id_estado INT NOT NULL,
-  PRIMARY KEY (id_tarea),
-  FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto),
-  FOREIGN KEY (id_responsable) REFERENCES usuario(id_usuario),
-  FOREIGN KEY (id_estado) REFERENCES estado(id_estado)
-);
-
-CREATE TABLE categoria
-(
-  id_categoria INT NOT NULL,
-  tipo_categoria VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id_categoria)
-);
-
-CREATE TABLE rol
-(
-  id_rol INT NOT NULL,
-  tipo_rol VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id_rol)
-);
-
-CREATE TABLE comentario
-(
-  descripcion VARCHAR(200) NOT NULL,
-  fecha_comentario DATE NOT NULL,
-  id_comentario INT NOT NULL,
-  id_tarea INT NOT NULL,
-  id_usuario INT NOT NULL,
-  PRIMARY KEY (id_comentario),
-  FOREIGN KEY (id_tarea) REFERENCES tarea(id_tarea),
-  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-);
-
-CREATE TABLE asignacion
-(
-  id_usuario INT NOT NULL,
-  id_proyecto INT NOT NULL,
-  id_rol INT NOT NULL,
-  PRIMARY KEY (id_usuario, id_proyecto),
-  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-  FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto),
-  FOREIGN KEY (id_rol) REFERENCES rol(id_rol)
-);
-
-CREATE TABLE tarea_categoria
-(
-  id_categoria INT NOT NULL,
-  id_tarea INT NOT NULL,
-  PRIMARY KEY (id_categoria, id_tarea),
-  FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
-  FOREIGN KEY (id_tarea) REFERENCES tarea(id_tarea)
-);
 
